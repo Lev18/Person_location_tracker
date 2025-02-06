3395a43b380f3161eaebe221ca2356bee1c73b0c
Levon <109511202+Lev18@users.noreply.github.com>
2025-02-06 22:28:05 +0400

    Update README.d

--- README.d
+++ README.d
@@ -1,4 +1,4 @@
-i didn't use IDE all were build in terminal
+I didn't use IDE all were build in terminal
 For build service you need run build.sh file it will give an output into out\ directory
 After building open two terminals in one you need run` java -cp "out:/home/levon/Kafka/kafka_2.13-3.9.0/libs/*:/home/levon/Downloads/psql/postgresql-42.6.0.jar" Consumer`
 and another one ` java -cp "out:/home/levon/Kafka/kafka_2.13-3.9.0/libs/*" Application`
