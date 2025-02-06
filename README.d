04ae656aedb50ffebb46e09102a0a99b3ca8be18
Levon <109511202+Lev18@users.noreply.github.com>
2025-02-06 23:46:05 +0400

    Update README.d

--- README.d
+++ README.d
@@ -1,6 +1,6 @@
 I didn't use IDE all were build in terminal
 For build service you need run build.sh file it will give an output into out\ directory
-After building open two terminals in one you need run` java -cp "out:/home/levon/Kafka/kafka_2.13-3.9.0/libs/*:/home/levon/Downloads/psql/postgresql-42.6.0.jar" Consumer`
-and another one ` java -cp "out:/home/levon/Kafka/kafka_2.13-3.9.0/libs/*" Application`
+After building open two terminals in one you need run` java -cp "out:/path/to/Kafka/libs/*:/path/to/psql/postgresql-42.6.0.jar" Consumer`
+and another one ` java -cp "out:/path/to/kafka/libs/*" Application`
 Application simulates random coordintes Consumer tracks, prepares report and saves them inside database
-
+""" Dont forget change `/path/to` into your path
