2ee8312feddc36e41c0ac57ec209df0ac24fd37f
Levon <109511202+Lev18@users.noreply.github.com>
2025-02-06 23:43:42 +0400

    Update buid.sh

--- buid.sh
+++ buid.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-javac -cp "/home/levon/Kafka/kafka_2.13-3.9.0/libs/*:/home/levon/Downloads/psql/postgresql-42.6.0.jar" -d out src/main/java/Consumer.java
-javac -cp "/home/levon/Kafka/kafka_2.13-3.9.0/libs/*" -d out src/main/java/Application.java
+javac -cp "/path/to/kafka/libs*:/path/to/psql/postgresql-42.6.0.jar" -d out src/main/java/Consumer.java
+javac -cp "/path/to/kafka/libs/*" -d out src/main/java/Application.java
